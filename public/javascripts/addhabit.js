bd01f559c739ad1e7a77b5675e4d87250f504ed6
ClaudiaShin <claudiashin07@gmail.com>
2021-10-05 15:13:04 -0700

    folder re-order

---
+++ public/javascripts/addhabit.js
@@ -0,0 +1,103 @@
+function healthButton() {
+
+    var health = document.getElementById("health");
+
+    health.style.backgroundColor = "#CCEBF2";
+    health.style.width = "40px";
+    health.style.height = "40px";
+    health.style.borderRadius = "50%";
+    health.style.boxShadow = "5px 5px 4px rgba(0, 0, 0, 0.25)"
+    health.style.padding = "15px"
+    
+}
+
+function fitButton() {
+
+    var fit = document.getElementById("fitness");
+
+    fit.style.backgroundColor = "#D2D2D2";
+    fit.style.width = "40px";
+    fit.style.height = "40px";
+    fit.style.borderRadius = "50%";
+    fit.style.boxShadow = "5px 5px 4px rgba(0, 0, 0, 0.25)"
+    fit.style.padding = "15px"
+    
+}
+
+function learnButton() {
+
+    var learn = document.getElementById("learn");
+
+    learn.style.backgroundColor = "#EEDBF3";
+    learn.style.width = "40px";
+    learn.style.height = "40px";
+    learn.style.borderRadius = "50%";
+    learn.style.boxShadow = "5px 5px 4px rgba(0, 0, 0, 0.25)"
+    learn.style.padding = "15px"
+    
+}
+
+function susButton() {
+
+    var sus = document.getElementById("sus");
+
+    sus.style.backgroundColor = "#E3F5D0";
+    sus.style.width = "40px";
+    sus.style.height = "40px";
+    sus.style.borderRadius = "50%";
+    sus.style.boxShadow = "5px 5px 4px rgba(0, 0, 0, 0.25)"
+    sus.style.padding = "15px"
+    
+}
+
+function houseButton() {
+
+    var house = document.getElementById("house");
+
+    house.style.backgroundColor = "#FEE4DE";
+    house.style.width = "40px";
+    house.style.height = "40px";
+    house.style.borderRadius = "50%";
+    house.style.boxShadow = "5px 5px 4px rgba(0, 0, 0, 0.25)"
+    house.style.padding = "15px"
+    
+}
+
+function relaxButton() {
+
+    var health = document.getElementById("health");
+
+    health.style.backgroundColor = "#CCEBF2";
+    health.style.width = "40px";
+    health.style.height = "40px";
+    health.style.borderRadius = "50%";
+    health.style.boxShadow = "5px 5px 4px rgba(0, 0, 0, 0.25)"
+    health.style.padding = "15px"
+    
+}
+
+function famButton() {
+
+    var health = document.getElementById("health");
+
+    health.style.backgroundColor = "#CCEBF2";
+    health.style.width = "40px";
+    health.style.height = "40px";
+    health.style.borderRadius = "50%";
+    health.style.boxShadow = "5px 5px 4px rgba(0, 0, 0, 0.25)"
+    health.style.padding = "15px"
+    
+}
+
+function finButton() {
+
+    var health = document.getElementById("health");
+
+    health.style.backgroundColor = "#CCEBF2";
+    health.style.width = "40px";
+    health.style.height = "40px";
+    health.style.borderRadius = "50%";
+    health.style.boxShadow = "5px 5px 4px rgba(0, 0, 0, 0.25)"
+    health.style.padding = "15px"
+    
+}

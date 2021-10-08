4effebebf8005a604d022ec642ee7368d268d36e
ClaudiaShin <claudiashin07@gmail.com>
2021-10-07 19:24:51 -0700

    add json ajax

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -1,29 +1,7 @@
-function popUp() {
-    var popup = document.getElementById('popupData');
-    popup.classList.toggle("show");
-
-    var inf = popup.value
-    const user = inf;
-
-    let text;
-    let info = prompt("Do you want to see your info?","");
-    
-    var signup = {"firstname":"tiger", "lastname":"woods", "email":"abc@abc.ca", "password":"this is password"}
-    var signupString = JSON.stringify(signup)
-
-    var signupObject = JSON.parse(signupString)
-
-    var newString = JSON.stringify(signupObject)
-
-    if(info == null || info == "") {
-        text = "No information added";
-    } else {
-        text = newString;
-    }
-    document.getElementById('popupData').innerHTML = text;
-
-
-
-
-
+function goToPage() {
+   
+    location.href = "./addhabit.html";
 }
+// document.getElementById("myButton").onclick = function () {
+//     location.href = "www.yoursite.com";
+// };
